ca36d9cccf85415a083ecd341b53055f112a300e
Jean-Étienne <git@jeanetienne.net>
2015-10-28 14:44:12 +1100

    Simplified the entry point

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-source ./src/test.sh
 source ./src/apps.sh
 source ./src/config.sh
 
-function launch_scripts
-{
-  # Ask for root password upfront
-  sudo -v
+# Ask for root password upfront
+sudo -v
 
-  # Installing apps
-  apps
-  
-  # Main setup
-  config
-}
+# Installing apps
+apps
 
-while true; do
-  echo "Make sure you have installed Xcode and launched it once."
-  echo ""
-  echo "Execute script with these settings? (y/n)"
-  read -p "$1" ANSWER
-  case $ANSWER in
-    [Yy]* ) launch_scripts; exit;;
-    [Nn]* ) exit;;
-    * ) echo "Please answer y or n.";;
-  esac
-done
+# Main setup
+config
+
+echo "Deploy script finished."
