c18ea1b5e656370e5d38cabd0e5f0f4c9d3e55b5
Jean-Étienne <parrotj1@corptst.anz.com>
2016-12-20 13:35:28 +1100

    Making hammerspoon window managment snappier

--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,4 @@
-local alertDuration = 0.35
+local alertDuration = 0.25
 
 -- Reload the config
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
@@ -12,15 +12,15 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "'", function()
     local windowFrame = window:frame()
     local screenFrame = window:screen():frame()
 
-	local shrinkingRatio = 0.08
-	local horizontalMargin = screenFrame.w * shrinkingRatio
-	local verticalMargin = screenFrame.h * shrinkingRatio
+    local shrinkingRatio = 0.08
+    local horizontalMargin = screenFrame.w * shrinkingRatio
+    local verticalMargin = screenFrame.h * shrinkingRatio
 
     windowFrame.x = screenFrame.x + horizontalMargin
     windowFrame.y = screenFrame.y + verticalMargin
     windowFrame.w = screenFrame.w - (horizontalMargin * 2)
     windowFrame.h = screenFrame.h - (verticalMargin * 2)
-    window:setFrame(windowFrame)
+    window:setFrame(windowFrame, 0)
 
 	hs.alert.show("Window: Center", alertDuration)
 end)
@@ -34,7 +34,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "return", function()
     windowFrame.y = screenFrame.y
     windowFrame.w = screenFrame.w
     windowFrame.h = screenFrame.h
-    window:setFrame(windowFrame)
+    window:setFrame(windowFrame, 0)
 
 	hs.alert.show("Window: Full screen", alertDuration)
 end)
@@ -49,7 +49,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
     windowFrame.y = screenFrame.y
     windowFrame.w = screenFrame.w
     windowFrame.h = screenFrame.h / 2
-    window:setFrame(windowFrame)
+    window:setFrame(windowFrame, 0)
 
 	hs.alert.show("Window: Top", alertDuration)
 end)
@@ -63,7 +63,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
     windowFrame.y = screenFrame.y
     windowFrame.w = screenFrame.w / 2
     windowFrame.h = screenFrame.h
-    window:setFrame(windowFrame)
+    window:setFrame(windowFrame, 0)
 
 	hs.alert.show("Window: Right", alertDuration)
 end)
@@ -77,7 +77,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
     windowFrame.y = screenFrame.y + (screenFrame.h / 2)
     windowFrame.w = screenFrame.w
     windowFrame.h = screenFrame.h / 2
-    window:setFrame(windowFrame)
+    window:setFrame(windowFrame, 0)
 
 	hs.alert.show("Window: Bottom", alertDuration)
 end)
@@ -91,24 +91,24 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
     windowFrame.y = screenFrame.y
     windowFrame.w = screenFrame.w / 2
     windowFrame.h = screenFrame.h
-    window:setFrame(windowFrame)
+    window:setFrame(windowFrame, 0)
 
 	hs.alert.show("Window: Left", alertDuration)
 end)
 
 -- Moving windows between screens
 hs.hotkey.bind({"alt", "ctrl"}, "Left", function()
-	local win = hs.window.focusedWindow()
-	win:moveToScreen(win:screen():previous())
+    local win = hs.window.focusedWindow()
+    win:moveToScreen(win:screen():previous(), false, false, 0)
 end)
 
 hs.hotkey.bind({"alt", "ctrl"}, "Right", function()
-	local win = hs.window.focusedWindow()
-	win:moveToScreen(win:screen():next())
+    local win = hs.window.focusedWindow()
+    win:moveToScreen(win:screen():next(), false, false, 0)
 end)
 
 -- Experiments
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", function()
-	local app = hs.application.frontmostApplication()
-	hs.tabs.enableForApp(app)
+    local app = hs.application.frontmostApplication()
+    hs.tabs.enableForApp(app)
 end)
