c20e63bbcf8eac848589e7077a442bf19db69af3
Jean-Étienne <git@jeanetienne.net>
2017-01-04 22:53:23 +1100

    Better cask config, removing Reveal, adding some QuickLook plugins, adding FiraCode font

--- src/config_cask.sh
+++ src/config_cask.sh
@@ -2,18 +2,26 @@
 
 function config_cask
 {
-	# Main Mac apps
-	brew cask install cyberduck
-	brew cask install dash
-	brew cask install dropbox
-	brew cask install flux
-	brew cask install hammerspoon
-	brew cask install paw
-	brew cask install reveal
-	brew cask install sourcetree
-	brew cask install textmate
-	brew cask install the-unarchiver
-	brew cask install vlc
+    # Main Mac apps
+    brew cask install cyberduck
+    brew cask install dash
+    brew cask install dropbox
+    brew cask install flux
+    brew cask install hammerspoon
+    brew cask install paw
+    brew cask install sourcetree
+    brew cask install textmate
+    brew cask install the-unarchiver
+    brew cask install vlc
+
+    # QuickLook plugins
+    brew cask install provisioning
+    brew cask install qlstephen # View plain text files without a file extension
+    brew cask install qlmarkdown
+
+    # Fonts
+    brew tap caskroom/fonts
+    brew cask install font-fira-code
 
 	echo "✅  finished configuring 'cask'"
 }
