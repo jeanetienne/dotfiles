535ad21ad54fcf7a6cfb030e735e65743b777a7a
Jean-Étienne <git@jeanetienne.net>
2017-01-04 22:32:34 +1100

    Better Git config

--- src/config_git.sh
+++ src/config_git.sh
@@ -2,15 +2,24 @@
 
 function config_git
 {
-  # User
-  git config --global user.email "git@jeanetienne.net"
-  git config --global user.name "Jean-Étienne"
-   
-  # Basic aliases
-  git config --global alias.co checkout;
-  git config --global alias.br branch;
-  git config --global alias.ci commit;
-  git config --global alias.st status;
+    # User
+    git config --global user.email "git@jeanetienne.net"
+    git config --global user.name "Jean-Étienne"
 
-  echo "✅  finished configuring 'Git'"
+    # Config
+    git config --global --add pull.default current
+    git config --global --add push.default current
+
+    # Basic aliases
+    git config --global alias.co checkout;
+    git config --global alias.br branch;
+    git config --global alias.ci commit;
+    git config --global alias.st status;
+    git config --global alias.cod checkout develop;
+    git config --global alias.rb rebase develop;
+    git config --global alias.rbi rebase -i develop;
+    git config --global alias.rbc rebase --continue;
+    git config --global alias.rba rebase --abort;
+
+    echo "✅  finished configuring 'Git'"
 }
