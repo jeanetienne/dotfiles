2968e802a57f47a91243591526b75c7d8a8796e4
Jean-Étienne <git@jeanetienne.net>
2017-01-04 21:53:30 +1100

    Adding post install message

--- src/config.sh
+++ src/config.sh
@@ -14,24 +14,18 @@ source ./src/config_hammerspoon.sh
 
 function config
 {
-  sudo -v
+    sudo -v
 
-  # Keep-alive: update existing 'sudo' time stamp until the whole script has finished
-  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+    # Keep-alive: update existing 'sudo' time stamp until the whole script has finished
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-  config_global
-  config_brew
-  config_default_apps
-  config_git
-  config_fish
-  config_cask
-  config_hammerspoon
+    config_global
+    config_brew
+    config_default_apps
+    config_git
+    config_fish
+    config_cask
+    config_hammerspoon
 
-  echo "Setup finished."
-  echo "Areas not covered by this script:"
-  echo "  - Setup the screensaver."
-  echo "  - Setup the clock and power icons in the menu bar."
-  echo "  - Set the default column width in column view."
-  echo ""
-  echo "Please restart."
+    cat post_install_message.txt
 }
