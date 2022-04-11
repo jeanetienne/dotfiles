396dbc6bfb4878e63760a291ff8069896149bd0a
Jean-Étienne <git@jeanetienne.net>
2022-04-11 10:25:01 +1000

    Updated Brew setup

--- src/config_brew.sh
+++ src/config_brew.sh
@@ -11,27 +11,6 @@ function config_brew
 
   # CLI apps
   brew bundle --file="./src/Brewfile"
-
-  brew install --cask caprine --appdir=~/Applications; 
-  brew install --cask cyberduck --appdir=~/Applications; 
-  brew install --cask daisydisk --appdir=~/Applications; 
-  brew install --cask dash --appdir=~/Applications; 
-  brew install --cask dropbox --appdir=~/Applications; 
-  brew install --cask fastlane --appdir=~/Applications; 
-  brew install --cask firefox --appdir=~/Applications; 
-  brew install --cask font-fira-code --appdir=~/Applications; 
-  brew install --cask fork --appdir=~/Applications; 
-  brew install --cask hammerspoon --appdir=~/Applications; 
-  brew install --cask harvest --appdir=~/Applications; 
-  brew install --cask nordvpn --appdir=~/Applications; 
-  brew install --cask paw --appdir=~/Applications; 
-  brew install --cask proxyman --appdir=~/Applications; 
-  brew install --cask slack --appdir=~/Applications; 
-  brew install --cask textmate --appdir=~/Applications; 
-  brew install --cask transmission --appdir=~/Applications; 
-  brew install --cask vlc --appdir=~/Applications; 
-  brew install --cask whatsapp --appdir=~/Applications; 
-  brew install --cask zeplin --appdir=~/Applications; 
-
+  
   echo "✅  finished configuring 'Brew'"
 }
